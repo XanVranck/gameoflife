ab6c68e0b3ec8df09e2b9e47081f85ee71f92b2b
Xan Vranckaert <xan.vranckaert@cegeka.com>
2017-06-17 16:26:04 +0200

    deleted fizzbuzz test & addign new tests

---
+++ src/test/java/be/cegeka/gameoflife/CellTest.java
@@ -0,0 +1,29 @@
+package be.cegeka.gameoflife;
+
+import org.junit.Test;
+
+/**
+ * Created by xanv on 17/06/2017.
+ */
+public class CellTest {
+    @Test
+    public void setIsAlive() throws Exception {
+
+    }
+
+    @Test
+    public void isAlive() throws Exception {
+
+    }
+
+    @Test
+    public void getRow() throws Exception {
+
+    }
+
+    @Test
+    public void getCol() throws Exception {
+
+    }
+
+}

95fc520b2ec5b21659c5ffa6b5b1c29dea7d3bbb
Xan Vranckaert <xan.vranckaert@cegeka.com>
2017-06-19 21:02:29 +0200

    Refactoring CellSurviveRules --> method names

--- src/main/java/be/cegeka/gameoflife/cell/CellSurviveRules.java
+++ src/main/java/be/cegeka/gameoflife/cell/CellSurviveRules.java
@@ -7,29 +7,32 @@ import java.util.List;
 
 public class CellSurviveRules {
     public Boolean doesCellLives(World world, Cell originalCell) {
-        List<Cell> grid = world.getGrid();
         List<Cell> neighbours = new ArrayList<>();
 
-        for (Cell cell : grid) {
-            if (cellIsInRangeOfRow(cell, originalCell) && cellIsInRangeOfCol(cell, originalCell) && !cell.equals(originalCell)) {
+        for (Cell cell : world.getGrid()) {
+            if (cellIsInRangeOfOriginalCell(originalCell, cell)) {
                 neighbours.add(cell);
             }
         }
         return determineIfCellLives(neighbours, originalCell);
     }
 
-    private boolean cellIsInRangeOfCol(Cell cell, Cell originalCell) {
+    private boolean cellIsInRangeOfOriginalCell(Cell originalCell, Cell cell) {
+        return cellIsInRangeOfOriginalCellRow(cell, originalCell) && cellIsInRangeOfOriginalCellCol(cell, originalCell) && !cell.equals(originalCell);
+    }
+
+    private boolean cellIsInRangeOfOriginalCellCol(Cell cell, Cell originalCell) {
         return cell.getCol() == originalCell.getCol() - 1 || cell.getCol() == originalCell.getCol() || cell.getCol() == originalCell.getCol() + 1;
     }
 
-    private boolean cellIsInRangeOfRow(Cell cell, Cell originalCell) {
+    private boolean cellIsInRangeOfOriginalCellRow(Cell cell, Cell originalCell) {
         return cell.getRow() == originalCell.getRow() - 1 || cell.getRow() == originalCell.getRow() || cell.getRow() == originalCell.getRow() + 1;
     }
 
     private Boolean determineIfCellLives(List<Cell>neighbours, Cell originalCell) {
         int amountOfLivingNeighbours = (int) neighbours.stream().filter(Cell::getIsAlive).count();
 
-        return originalCellWasAllreadyAliveAndHasTwoOrThreeNeighbours(originalCell, amountOfLivingNeighbours) ||
+        return originalCellWasAliveAndHasTwoOrThreeNeighbours(originalCell, amountOfLivingNeighbours) ||
                 OriginalCellWasDeadAndHasExactlyThreeNeighbours(originalCell, amountOfLivingNeighbours);
     }
 
@@ -37,7 +40,7 @@ public class CellSurviveRules {
         return !originalCell.getIsAlive() && amountOfLivingNeighbours == 3;
     }
 
-    private boolean originalCellWasAllreadyAliveAndHasTwoOrThreeNeighbours(Cell originalCell, int amountOfLivingNeighbours) {
+    private boolean originalCellWasAliveAndHasTwoOrThreeNeighbours(Cell originalCell, int amountOfLivingNeighbours) {
         return originalCell.getIsAlive() && (amountOfLivingNeighbours == 2 || amountOfLivingNeighbours == 3);
     }
 }
